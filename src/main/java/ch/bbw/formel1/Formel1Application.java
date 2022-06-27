65f7e791ab817fab21fac3e882902aecdddcdad2
Noel <noel.gebhardt@gmail.com>
2022-06-27 16:34:02 +0200

    Formel1

---
+++ src/main/java/ch/bbw/formel1/Formel1Application.java
@@ -0,0 +1,13 @@
+package ch.bbw.formel1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Formel1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Formel1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bbw/formel1/Formel1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.formel1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Formel1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
